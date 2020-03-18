a2f52b053c0d360fdd9070207437f0fd3789a81b
Hal Shin <hal.shin@alumni.ubc.ca>
2020-03-17 22:35:08 -0700

    Change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const steamUrl = "https://steamcommunity.com/id/";
 const trackerUrl = "https://rocketleague.tracker.network/profile/steam/";
