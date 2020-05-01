a15ea843c2b44967173fbe7d42490f89de2fb679
Hal Shin <hal.shin@alumni.ubc.ca>
2020-05-01 11:08:46 -0700

    Reset initial data

--- client/src/contexts/DialogContext.jsx
+++ client/src/contexts/DialogContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const DialogContext = createContext();
 
 export function DialogProvider(props) {
-  const [open, setOpen] = useState("player-info");
+  const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState("loading"); // only options are "loading" and "error"
   const [chatOpen, setChatOpen] = useState(null);
   const [openPlayerContextMenu, setOpenPlayerContextMenu] = useState({
@@ -11,22 +11,7 @@ export function DialogProvider(props) {
     mouseY: null
   });
   const [fetchedPlayer, setFetchedPlayer] = useState({});
-  const [currentPlayerInfo, setCurrentPlayerInfo] = useState({
-    id: "thewarriorofblue",
-    platform: "steam",
-    steamProfile: "https://steamcommunity.com/id/thewarriorofblue",
-    trackerProfile:
-      "https://rocketleague.tracker.network/profile/steam/thewarriorofblue",
-    success: true,
-    tag: "TheWarriorOfBlue",
-    icon:
-      "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e8/e849ef2a16c58c095a0434d6c48b2740e42e3579_full.jpg",
-    ranks: {
-      currentSeason: { ones: 1102, twos: 1566, threes: 1514 },
-      lastSeason: { ones: 1117, twos: 1733, threes: 1804 }
-    },
-    verified: true
-  }); // check usage of this
+  const [currentPlayerInfo, setCurrentPlayerInfo] = useState(null); // check usage of this
 
   return (
     <DialogContext.Provider
