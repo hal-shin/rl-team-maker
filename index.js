a2f4f20b8802671b3c80fb65b0b4414277523cb0
Hal Shin <hal.shin@alumni.ubc.ca>
2020-05-03 21:44:11 -0700

    alter index.html pathing

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ server.listen(port, () => {
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -144,5 +148,5 @@ app.post("/session", (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
