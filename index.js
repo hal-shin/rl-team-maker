76a4adea0cbc04402fd7977b733a5a2c4f9b5823
Hal Shin <hal.shin@alumni.ubc.ca>
2020-05-04 18:52:28 -0700

    readjust mongo connectionn code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -36,127 +36,125 @@ mongoose.connect(
   (err, connection) => {
     if (err) console.log("Database error:", err);
     else {
-      if (process.env.NODE_ENV === "development") {
-        console.log("Successful database connection");
-      }
-
-      // socket logic
-      io.on("connection", client => {
-        client.emit("general-connect");
-
-        client.on("join-session", payload => {
-          const { sessionUrl, newSessionId } = payload;
-          console.log("Someone joined", sessionUrl);
-          client.join(sessionUrl);
-
-          Session.findById(newSessionId, (err, foundSession) => {
-            if (err) return console.log("SESSION NOT FOUND");
-            else {
-              const newStore = JSON.parse(foundSession.store);
-
-              client.emit("update-board", newStore.board);
-              client.emit("update-session", foundSession.session);
-            }
-          });
-        });
-
-        client.on("board-changed", payload => {
-          const { sessionUrl, sessionId, newStore } = payload;
-          // check if emitter is the host
-          Session.findById(sessionId, (err, foundSession) => {
-            if (err) return console.log("SESSION NOT FOUND");
-            else {
-              if (foundSession.session.hostUrl === sessionUrl) {
-                Session.findByIdAndUpdate(
-                  sessionId,
-                  { store: JSON.stringify(newStore) },
-                  (err, updatedStore) => {
-                    if (err) console.log("Session data update failed.");
-                  }
-                );
-                client
-                  .to(foundSession.session.viewerUrl)
-                  .emit("update-board", newStore.board);
-                client
-                  .to(foundSession.session.hostUrl)
-                  .emit("update-board", newStore.board);
-              }
-            }
-          });
-        });
-      });
-
-      // Tracker route
-      app.use("/search", tracker);
-
-      // Retrieve session ID based on given URL
-      app.get("/session", (req, res) => {
-        let isViewer = true;
-        const sessionUrl = req.query.url;
-
-        if (!sessionUrl) {
-          res.status(400);
-          res.send("Please input a session URL");
-        } else if (sessionUrl) {
-          Session.findOne({ urls: sessionUrl }, (err, foundSession) => {
-            if (err) {
-              res.status(404);
-              res.send("No such session found");
-            } else {
-              // console.log("FOUND SESSION:", foundSession);
-              if (foundSession.session.hostUrl === sessionUrl) isViewer = false;
-              res.json({ sessionId: foundSession._id, isViewer });
-            }
-          });
-        }
-      });
-
-      app.post("/session", (req, res) => {
-        let { newBoard, storeData, boardData, hostName } = req.body;
-
-        hostName = hostName || "Admin";
-        newBoard = newBoard || false;
-
-        const newHostUrl = randomString.generate({
-          length: 8,
-          charset: "alphabetic"
-        });
-        const newViewerUrl = randomString.generate({
-          length: 8,
-          charset: "alphabetic"
-        });
-
-        if (newBoard === true) {
-          boardData = blankBoard;
-        }
-
-        const newSession = new Session({
-          urls: [newHostUrl, newViewerUrl],
-          session: {
-            hostUrl: newHostUrl,
-            viewerUrl: newViewerUrl,
-            connected: true,
-            hostName
-          },
-          store: JSON.stringify(storeData)
-        });
-
-        newSession.save((err, doc) => {
-          if (err) {
-            console.log(err);
-            res.status(400);
-            res.send("something went wrong.");
-          } else {
-            res.json(doc);
-          }
-        });
-      });
-
-      // The "catchall" handler: for any request that doesn't
-      // match one above, send back React's index.html file.
-      app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "/client/public", "index.html"));
-      });
+      console.log("Successful database connection");
     }
   }
 );
+
+// socket logic
+io.on("connection", client => {
+  client.emit("general-connect");
+
+  client.on("join-session", payload => {
+    const { sessionUrl, newSessionId } = payload;
+    console.log("Someone joined", sessionUrl);
+    client.join(sessionUrl);
+
+    Session.findById(newSessionId, (err, foundSession) => {
+      if (err) return console.log("SESSION NOT FOUND");
+      else {
+        const newStore = JSON.parse(foundSession.store);
+
+        client.emit("update-board", newStore.board);
+        client.emit("update-session", foundSession.session);
+      }
+    });
+  });
+
+  client.on("board-changed", payload => {
+    const { sessionUrl, sessionId, newStore } = payload;
+    // check if emitter is the host
+    Session.findById(sessionId, (err, foundSession) => {
+      if (err) return console.log("SESSION NOT FOUND");
+      else {
+        if (foundSession.session.hostUrl === sessionUrl) {
+          Session.findByIdAndUpdate(
+            sessionId,
+            { store: JSON.stringify(newStore) },
+            (err, updatedStore) => {
+              if (err) console.log("Session data update failed.");
+            }
+          );
+          client
+            .to(foundSession.session.viewerUrl)
+            .emit("update-board", newStore.board);
+          client
+            .to(foundSession.session.hostUrl)
+            .emit("update-board", newStore.board);
+        }
+      }
+    });
+  });
+});
+
+// Tracker route
+app.use("/search", tracker);
+
+// Retrieve session ID based on given URL
+app.get("/session", (req, res) => {
+  let isViewer = true;
+  const sessionUrl = req.query.url;
+
+  if (!sessionUrl) {
+    res.status(400);
+    res.send("Please input a session URL");
+  } else if (sessionUrl) {
+    Session.findOne({ urls: sessionUrl }, (err, foundSession) => {
+      if (err) {
+        res.status(404);
+        res.send("No such session found");
+      } else {
+        // console.log("FOUND SESSION:", foundSession);
+        if (foundSession.session.hostUrl === sessionUrl) isViewer = false;
+        res.json({ sessionId: foundSession._id, isViewer });
+      }
+    });
+  }
+});
+
+app.post("/session", (req, res) => {
+  let { newBoard, storeData, boardData, hostName } = req.body;
+
+  hostName = hostName || "Admin";
+  newBoard = newBoard || false;
+
+  const newHostUrl = randomString.generate({
+    length: 8,
+    charset: "alphabetic"
+  });
+  const newViewerUrl = randomString.generate({
+    length: 8,
+    charset: "alphabetic"
+  });
+
+  if (newBoard === true) {
+    boardData = blankBoard;
+  }
+
+  const newSession = new Session({
+    urls: [newHostUrl, newViewerUrl],
+    session: {
+      hostUrl: newHostUrl,
+      viewerUrl: newViewerUrl,
+      connected: true,
+      hostName
+    },
+    store: JSON.stringify(storeData)
+  });
+
+  newSession.save((err, doc) => {
+    if (err) {
+      console.log(err);
+      res.status(400);
+      res.send("something went wrong.");
+    } else {
+      res.json(doc);
+    }
+  });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/public", "index.html"));
+});
